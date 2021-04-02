42077d0324094bd085918927df2cbae8e1de6485
Schimmoellerm <schimmoellerm@gmail.com>
2021-04-02 09:32:22 -0400

    added inquirer prompts for team name and manager information

--- index.js
+++ index.js
@@ -7,5 +7,26 @@ const fs = require('fs');
 
 let teamName;
 let manager;
-let engineers = [];
-let interns = [];
+let engineersArr = [];
+let internsArr = [];
+
+const startTeam = () => {
+    inquirer
+        .prompt([
+            {type: 'input', message: 'What is the name of your team?', name: 'teamName'},
+
+            {type: 'input', message: 'What is the name of the manager?', name: 'managerName'},
+
+            {type: 'input', message: `What is the manager's id?`, name: 'managerId'},
+
+            {type: 'input', message: `Waht is the manager's email?`, name: 'managerEmail'},
+
+            {type: 'input', message: `What is the manager's office number?`, name: 'managerOffice'},
+        ])
+        .then(answers => {
+            teamName = answers.teamName;
+            manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice)
+            console.log(teamName, manager)
+        })
+}
+startTeam()
